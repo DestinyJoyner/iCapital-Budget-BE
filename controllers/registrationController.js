94cabf8d03dfd032e8837bd5f7323e8c785b25b7
DestinyJoyner <destinyjoyner@pursuit.org>
2024-11-27 23:27:57 -0500

    create user querie function added and post route for registration with middleware anf=d jwt generation added

--- controllers/registrationController.js
+++ controllers/registrationController.js
@@ -1,12 +1,36 @@
-const express = require("express")
-const registration = express.Router()
+const express = require("express");
+const registration = express.Router();
+const {
+  isEmailUnique,
+  hashPass,
+  generateJWT,
+} = require("../middleware/authorization.js");
+const { createUser } = require("../queries/registration.js");
 
+registration.get("/", (req, res) => {
+  res.status(200).json("Registration Page");
+});
 
-registration.get("/", (req,res) => {
-    res.status(200).json(
-        "Registration Page"
-    )
-})
+// CREATE NEW USER
+registration.post("/", isEmailUnique, hashPass, async (req, res) => {
+  const newUser = await createUser(req.body.login);
 
+  if (!newUser.message) {
+    try {
+      const email = newUser.email;
+      const token = generateJWT(email);
+      newUser.token = token;
+      res.status(200).json(newUser);
+    } catch (jwtErr) {
+      res.status(500).json({
+        error: jwtErr,
+      });
+    }
+  } else {
+    res.status(500).json({
+      error: newUser.message,
+    });
+  }
+});
 
-module.exports = registration
+module.exports = registration;
--- middleware/authorization.js
+++ middleware/authorization.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcrypt");
 const JWT = require("jsonwebtoken");
 const dotenv = require("dotenv");
 dotenv.config();
-const {checkEmail} = require("../queries/emails.js")
+const { checkEmail } = require("../queries/emails.js");
 
 /* 
     req shape = req {
@@ -25,7 +25,6 @@ async function hashPass(req, res, next) {
     const hashedPass = await bcrypt.hash(password, salt);
     req.body.login.password = hashedPass;
     next();
-
   } catch (err) {
     res.status(404).json({
       error: err,
@@ -36,39 +35,41 @@ async function hashPass(req, res, next) {
 // generate JWT
 //  1-24 hours after developemtn for expiresIn val
 
-function generateJWT (userEmail) {
-    const token = JWT.sign({email : userEmail}, process.env.SECRET_TOKEN, {
-        expiresIn: "240h"
-    })
+function generateJWT(userEmail) {
+  const token = JWT.sign({ email: userEmail }, process.env.SECRET_TOKEN, {
+    expiresIn: "240h",
+  });
 
-    return token 
+  return token;
 }
 
 // function for validating email
 
-async function isEmailUnique (req,res,next) {
+async function isEmailUnique(req, res, next) {
   try {
-    const user_email = req.body.login.email
-    const isUnique = await checkEmail(user_email)
+    const user_email = req.body.login.email;
+    const isUnique = await checkEmail(user_email);
 
-    if(isUnique.error){
-        res.status(500).json({error: "Database error checking email"})
+    if (isUnique.error) {
+      res.status(500).json({ error: "Database error checking email" });
     }
 
-    if(!isUnique){
-        res.status(400).json({error: `${user_email} is linked to another account`})
+    if (!isUnique) {
+      res
+        .status(400)
+        .json({ error: `${user_email} is linked to another account` });
+    } else {
+      next();
     }
-    else {
-        next()
-    }
-
-  } catch(err){
+  } catch (err) {
     res.status(500).json({
-        error: "Error checking email"
-    })
+      error: "Error checking email",
+    });
   }
-
-
 }
 
-
+module.exports = {
+    isEmailUnique,
+    hashPass,
+    generateJWT
+}
