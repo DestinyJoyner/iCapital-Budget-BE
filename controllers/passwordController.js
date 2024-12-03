d36ccbfc08b038dc1ff54a585855290d5eb144d0
DestinyJoyner <destinyjoyner@pursuit.org>
2024-12-02 21:13:05 -0500

    implemented token cleanup function and verification token route for password reset

--- controllers/passwordController.js
+++ controllers/passwordController.js
@@ -1,69 +1,94 @@
 const express = require("express");
 const password = express.Router();
-const {checkEmail} = require("../queries/emails.js")
-const {generateCryptoToken} = require("../middleware/authorization.js")
-const {storeVerificationToken} = require("../queries/verification.js")
+const { checkEmail } = require("../queries/emails.js");
+const { generateCryptoToken } = require("../middleware/authorization.js");
+const {
+  storeVerificationToken,
+  isValidToken,
+} = require("../queries/verification.js");
 const ejs = require("ejs");
 const path = require("path");
 const transporter = require("../config/mailerConfig.js");
 require("dotenv").config();
 
-// get route to initiate password reset -> receives email in body, used for verification if in DB
+// route to verify validity of the token AFTER initializing password reset process => then FE form submission of new password route to PUT route
+
+password.get("/verify-token/:verificationToken", async (req, res) => {
+  try {
+    const { verificationToken } = req.params;
+    const validTokenEmail = await isValidToken(verificationToken);
+
+    if (!validTokenEmail || validTokenEmail.message) {
+      res.status(400).json({
+        error: "Invalid or expired reset link. Request a new one.",
+        msg: validTokenEmail?.message,
+      });
+    } else {
+      res.status(200).json({
+        message: "Valid Token",
+        email: validTokenEmail.email,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      error: "Error verifying reset token",
+    });
+  }
+});
+
+// post route to initiate password reset -> receives email in body, used for verification if in DB
 password.post("/", async (req, res) => {
-    const {email} = req.body
-    const isValidEmail = await checkEmail(email)
-    
-    // if email in db, generate verification token and send to email
-    if(isValidEmail){
-        try {
-            const verificationToken = generateCryptoToken()
-            const userVerifiedObj = await storeVerificationToken(verificationToken, email)
-            if(!userVerifiedObj.message){
-                const {id, email, verification_token} = userVerifiedObj
+  const { email } = req.body;
+  const isValidEmail = await checkEmail(email);
 
-            const verification_link = `${process.env.FRONT_END_URL}/password-reset/${verification_token}`
+  // if email in db, generate verification token and send to email
+  if (isValidEmail) {
+    try {
+      const verificationToken = generateCryptoToken();
+      const userVerifiedObj = await storeVerificationToken(
+        verificationToken,
+        email
+      );
+      if (!userVerifiedObj.message) {
+        const { id, email, verification_token } = userVerifiedObj;
 
+        const verification_link = `${process.env.FRONT_END_URL}/password-reset/${verification_token}`;
 
-                const emailBody = await ejs.renderFile(
-                    path.join(__dirname, "../data/emailTemplate.ejs"),
-                    {
-                      template: "password_reset",
-                      details: {verification_link}
-                    }
-                  );
-          
-                  //   send email
-                  await transporter.sendMail({
-                    from: process.env.EMAIL_USER,
-                    to: email,
-                    subject: "iCapital Budget Account Password Reset",
-                    html: emailBody,
-                  });
+        const emailBody = await ejs.renderFile(
+          path.join(__dirname, "../data/emailTemplate.ejs"),
+          {
+            template: "password_reset",
+            details: { verification_link },
+          }
+        );
 
-                  res.status(200).json({
-                    message: "Password reset link has been sent"
-                  })
-            }
-            else {
-                res.status(500).json({error: "Server Error", msg: userVerifiedObj.message})
-            }
-            
+        //   send email
+        await transporter.sendMail({
+          from: process.env.EMAIL_USER,
+          to: email,
+          subject: "iCapital Budget Account Password Reset",
+          html: emailBody,
+        });
 
-        }catch(err){
-            res.status(500).json({
-                error: "Error verifying email"
-            })
-        }
-        
+        res.status(200).json({
+          message: "Password reset link has been sent",
+        });
+      } else {
+        res
+          .status(500)
+          .json({ error: "Server Error", msg: userVerifiedObj.message });
+      }
+    } catch (err) {
+      res.status(500).json({
+        error: "Error verifying email",
+      });
     }
-    else {
-        res.status(500).json({
-            error: "Email not linked to any account",
-            message: isValidEmail.message
-            
-        })
-    }
+  } else {
+    res.status(500).json({
+      error: "Email not linked to any account",
+      message: isValidEmail.message,
+    });
+  }
+});
 
-})
-
-module.exports = password
+module.exports = password;
--- queries/verification.js
+++ queries/verification.js
@@ -15,6 +15,8 @@ async function verifyUser (email, verificationToken) {
 // store verification token in user database
 async function storeVerificationToken (verificationToken, userEmail){
     try{
+        // cleanup expired tokens
+        await removeExpiredTokens()
         // set time for token expiration -> 1hr
         const expirationTime = new Date(Date.now() +3600000).toISOString()
 
@@ -27,7 +29,32 @@ async function storeVerificationToken (verificationToken, userEmail){
     }
 } 
 
+// verification token cleanup -> run check if expiration timestamp has past (< Current time)
+async function removeExpiredTokens() {
+    try {
+        // no response needed just do it db.none()
+        await db.none("UPDATE icapital_users SET verification_token = NULL, verification_token_expiration = NULL WHERE verification_token_expiration < CURRENT_TIMESTAMP");
+
+    } catch(err) {
+        return err;
+    }
+}
+
+// query for checking verification token validity
+async function isValidToken (verificationToken) {
+    try{
+        // return email of corresponding email account
+        const validTokenEmail = await db.oneOrNone("SELECT email FROM icapital_users WHERE verification_token = $1 AND verification_token_expiration > CURRENT_TIMESTAMP", verificationToken)
+
+        return validTokenEmail
+
+    }catch(err) {
+        return err
+    }
+}
+
 module.exports = {
     verifyUser,
-    storeVerificationToken
+    storeVerificationToken,
+    isValidToken
 }
