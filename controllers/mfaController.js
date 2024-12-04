286281827718f94574f77aa3abd25da5bddcd8d0
DestinyJoyner <destinyjoyner@pursuit.org>
2024-12-04 13:19:50 -0500

    verify update passcode and login functionality updated, functioning

--- controllers/mfaController.js
+++ controllers/mfaController.js
@@ -3,9 +3,11 @@ const mfa = express.Router()
 const {generatePasscode, verifyPasscode, authorizeUser} = require("../middleware/authorization.js")
 const {storePasscode} = require("../queries/mfa.js")
 const {sendEmail, emailTemplate} = require("../config/mailerConfig.js")
+const {signInSchema} = require("../middleware/validators/userValidators.js")
+const {validationError} = require("../middleware/validators/errorValidator.js")
 
 // /auth/mfa/login
-mfa.post("/login", authorizeUser, async (req,res) => {
+mfa.post("/login", signInSchema, validationError, authorizeUser, async (req,res) => {
    try {
      // from authorize user
      const {user_id} = req.body
@@ -15,6 +17,7 @@ mfa.post("/login", authorizeUser, async (req,res) => {
      // speakeasy - generate secret, store in table for user
      const userPasscode = generatePasscode()
      const storedUserPasscode = await storePasscode(user_id, userPasscode)
+    //  console.log(storedUserPasscode, "store passcode")
  
      // send email
      const emailBody = await emailTemplate("passcode", {passcode:userPasscode})
@@ -23,6 +26,8 @@ mfa.post("/login", authorizeUser, async (req,res) => {
  
      res.status(200).json({
          message: "Passcode email sent",
+         user_id: user_id,
+       
      })
  
    }catch(err){
--- queries/mfa.js
+++ queries/mfa.js
@@ -5,7 +5,7 @@ async function storePasscode(userId, passcode) {
     try{
         const expirationTime = new Date(Date.now() +3600000).toISOString()
 
-       const storedPasscode = await db.one("UPDATE icapital_users SET one_time_passcode = $1 one_time_passcode_expiration = $2 WHERE id = $3 RETURNING *", [passcode, expirationTime,userId])
+       const storedPasscode = await db.one("UPDATE icapital_users SET one_time_passcode = $1, one_time_passcode_expiration = $2 WHERE id = $3 RETURNING *", [passcode, expirationTime,userId])
        
        return storedPasscode
 
@@ -16,14 +16,30 @@ async function storePasscode(userId, passcode) {
 
 async function verifyPasscode (userId, passcode) {
     try{
-        const isValid = await db.oneOrNone("SELECT id FROM icapital_users WHERE id =$1 AND on_time_passcode = $2 AND one_time_passcode_expiration > CURRENT_TIMESTAMP ", [userId, passcode])
+        const isValid = await db.oneOrNone("SELECT id FROM icapital_users WHERE id =$1 AND one_time_passcode = $2 AND one_time_passcode_expiration > CURRENT_TIMESTAMP ", [userId, passcode])
+
+        // clear passcode
+        if(isValid){
+            await removePasscode(userId)
+        }
         //  return boolean for ease use !!
         return !!isValid
 
     }catch(err){
+        console.log("verify passcode err", err)
         return err
     }
 }
+
+async function removePasscode (userId) {
+    try {
+        await db.none("UPDATE icapital_users SET one_time_passcode = NULL, one_time_passcode_expiration = NULL WHERE id= $1", userId)
+    }catch(err){
+        return err
+    }
+}
+
+
 
 module.exports = {
     storePasscode,
